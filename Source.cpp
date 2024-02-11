19b3a717fe6e3b131bf368356993bf4154ebef78
Rimsha110 <bsef20a001@pucit.edu.pk>
2024-02-11 12:31:59 +0500

    Version2

--- Source.cpp
+++ Source.cpp
@@ -1,128 +1,103 @@
 #include <iostream>
-#include <cmath> //SQRT function
-using namespace std;
+#include <cmath>
 
-class Calculator
-{
-private:
+class CalculatorBase {
+protected:
     float val;
 
 public:
-    // Constructor to initialize the Calculator with an initial value
-    Calculator(float initialValue) : val(initialValue)
-    {
-    }
+    CalculatorBase(float initialValue) : val(initialValue) {}
 
-    // Function to perform addition
-    float add(float num)
-    {
-        val += num;
-        return val;
-    }
-
-    // Function to perform subtraction
-    float subtract(float num)
-    {
-        val -= num;
-        return val;
-    }
-
-    // Function to perform multiplication
-    float multiply(float num)
-    {
-        val *= num;
-        return val;
-    }
-
-    // Function to perform integer division
-    int divide(int num)
-    {
-        if (num != 0)
-        {
-            val /= num;
-            return static_cast<int>(val);
-        }
-        else
-        {
-            cout << "Error: Cannot divide by zero!!" << endl;
-            return static_cast<int>(val);
-        }
-    }
-
-    // Function to calculate the power of the current value to a given exponent
-    void power(int exponent)
-    {
-        val = pow(val, exponent);
-    }
-
-    // Function to calculate the square root of the current value
-    float squareRoot()
-    {
-        if (val >= 0)
-        {
-            val = sqrt(val);
-            return val;
-        }
-        else
-        {
-            cout << "Error: Cannot calculate square root of a negative number!!" << endl;
-            return val;
-        }
-    }
-
-    // Function to negate the current value
-    float negate()
-    {
-        val = -val;
-        return val;
-    }
-
-    // Function to check if the current value is even or odd
-    void checkEvenOdd()
-    {
-        if (static_cast<int>(val) % 2 == 0)
-        {
-            cout << "The current value is even." << endl;
-        }
-        else
-        {
-            cout << "The current value is odd." << endl;
-        }
-    }
-
-    // Function to get the current value
-    float getValue() const
-    {
+    float getValue() const {
         return val;
     }
 };
 
-int main()
-{
+class Calculator : public CalculatorBase {
+public:
+    Calculator(float initialValue) : CalculatorBase(initialValue) {}
+
+    float add(float num) {
+        val += num;
+        return val;
+    }
+
+    float subtract(float num) {
+        val -= num;
+        return val;
+    }
+
+    float multiply(float num) {
+        val *= num;
+        return val;
+    }
+
+    int divide(int num) {
+        if (num != 0) {
+            val /= num;
+            return static_cast<int>(val);
+        }
+        else {
+            std::cout << "Error: Cannot divide by zero!!" << std::endl;
+            return static_cast<int>(val);
+        }
+    }
+
+    void power(int exponent) {
+        val = std::pow(val, exponent);
+    }
+
+    float squareRoot() {
+        if (val >= 0) {
+            val = std::sqrt(val);
+            return val;
+        }
+        else {
+            std::cout << "Error: Cannot calculate square root of a negative number!!" << std::endl;
+            return val;
+        }
+    }
+
+    float negate() {
+        val = -val;
+        return val;
+    }
+
+    void checkEvenOdd() {
+        if (static_cast<int>(val) % 2 == 0) {
+            std::cout << "The current value is even." << std::endl;
+        }
+        else {
+            std::cout << "The current value is odd." << std::endl;
+        }
+    }
+};
+
+int main() {
     // Create an instance of the Calculator class with an initial value of 5.0
     Calculator myCal(5.0);
 
     // Perform various operations and display the results
     myCal.add(5.0);
-    cout << "After adding 5, Value: " << myCal.getValue() << endl;
+    std::cout << "After adding 5, Value: " << myCal.getValue() << std::endl;
 
     myCal.subtract(3.0);
-    cout << "After subtracting 3, Value: " << myCal.getValue() << endl;
+    std::cout << "After subtracting 3, Value: " << myCal.getValue() << std::endl;
 
     myCal.multiply(2.0);
-    cout << "After multiplying by 2, Value: " << myCal.getValue() << endl;
+    std::cout << "After multiplying by 2, Value: " << myCal.getValue() << std::endl;
 
     myCal.divide(4);
-    cout << "After dividing by 4 (integer division), Value: " << myCal.getValue() << endl;
+    std::cout << "After dividing by 4 (integer division), Value: " << myCal.getValue() << std::endl;
 
     myCal.power(2);
-    cout << "After raising to the power of 2, Value: " << myCal.getValue() << endl;
+    std::cout << "After raising to the power of 2, Value: " << myCal.getValue() << std::endl;
 
     myCal.squareRoot();
-    cout << "After calculating square root, Value: " << myCal.getValue() << endl;
+    std::cout << "After calculating square root, Value: " << myCal.getValue() << std::endl;
 
     myCal.negate();
-    cout << "After negating, Value: " << myCal.getValue() << endl;
+    std::cout << "After negating, Value: " << myCal.getValue() << std::endl;
 
     // New function to check if the current value is even or odd
     myCal.checkEvenOdd();
