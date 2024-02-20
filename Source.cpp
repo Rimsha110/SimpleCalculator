f2967c0f8170dc64ece58b70ddbff207d8691e9b
Rimsha110 <bsef20a001@pucit.edu.pk>
2024-02-20 09:34:26 +0500

    Version3

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,41 @@
+
 #include <iostream>
 #include <cmath>
 
-class CalculatorBase {
+class OperationBase {
 protected:
     float val;
 
 public:
-    CalculatorBase(float initialValue) : val(initialValue) {}
+    OperationBase(float initialValue) : val(initialValue) {}
 
     float getValue() const {
         return val;
     }
 };
 
-class Calculator : public CalculatorBase {
+class UnaryOperation : public OperationBase {
 public:
-    Calculator(float initialValue) : CalculatorBase(initialValue) {}
+    UnaryOperation(float initialValue) : OperationBase(initialValue) {}
+
+    float negate() {
+        val = -val;
+        return val;
+    }
+
+    void checkEvenOdd() {
+        if (static_cast<int>(val) % 2 == 0) {
+            std::cout << "The current value is even." << std::endl;
+        }
+        else {
+            std::cout << "The current value is odd." << std::endl;
+        }
+    }
+};
+
+class BinaryOperation : public OperationBase {
+public:
+    BinaryOperation(float initialValue) : OperationBase(initialValue) {}
 
     float add(float num) {
         val += num;
@@ -57,50 +77,36 @@ public:
             return val;
         }
     }
-
-    float negate() {
-        val = -val;
-        return val;
-    }
-
-    void checkEvenOdd() {
-        if (static_cast<int>(val) % 2 == 0) {
-            std::cout << "The current value is even." << std::endl;
-        }
-        else {
-            std::cout << "The current value is odd." << std::endl;
-        }
-    }
 };
 
 int main() {
-    // Create an instance of the Calculator class with an initial value of 5.0
-    Calculator myCal(5.0);
+    UnaryOperation myUnaryOperation(5.0);
+    BinaryOperation myBinaryOperation(5.0);
 
     // Perform various operations and display the results
-    myCal.add(5.0);
-    std::cout << "After adding 5, Value: " << myCal.getValue() << std::endl;
+    myBinaryOperation.add(5.0);
+    std::cout << "After adding 5, Value: " << myBinaryOperation.getValue() << std::endl;
 
-    myCal.subtract(3.0);
-    std::cout << "After subtracting 3, Value: " << myCal.getValue() << std::endl;
+    myBinaryOperation.subtract(3.0);
+    std::cout << "After subtracting 3, Value: " << myBinaryOperation.getValue() << std::endl;
 
-    myCal.multiply(2.0);
-    std::cout << "After multiplying by 2, Value: " << myCal.getValue() << std::endl;
+    myBinaryOperation.multiply(2.0);
+    std::cout << "After multiplying by 2, Value: " << myBinaryOperation.getValue() << std::endl;
 
-    myCal.divide(4);
-    std::cout << "After dividing by 4 (integer division), Value: " << myCal.getValue() << std::endl;
+    myBinaryOperation.divide(4);
+    std::cout << "After dividing by 4 (integer division), Value: " << myBinaryOperation.getValue() << std::endl;
 
-    myCal.power(2);
-    std::cout << "After raising to the power of 2, Value: " << myCal.getValue() << std::endl;
+    myBinaryOperation.power(2);
+    std::cout << "After raising to the power of 2, Value: " << myBinaryOperation.getValue() << std::endl;
 
-    myCal.squareRoot();
-    std::cout << "After calculating square root, Value: " << myCal.getValue() << std::endl;
+    myBinaryOperation.squareRoot();
+    std::cout << "After calculating square root, Value: " << myBinaryOperation.getValue() << std::endl;
 
-    myCal.negate();
-    std::cout << "After negating, Value: " << myCal.getValue() << std::endl;
+    myUnaryOperation.negate();
+    std::cout << "After negating, Value: " << myUnaryOperation.getValue() << std::endl;
 
     // New function to check if the current value is even or odd
-    myCal.checkEvenOdd();
+    myUnaryOperation.checkEvenOdd();
 
     return 0;
 }
